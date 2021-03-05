0dfa0b20f0c1095a963574827a2a325d34929efb
Apple <apple@Apples-Mac-mini.local>
2021-03-05 10:54:50 +0530

    Api Call Sucess

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,48 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Demo demo1;
+  @override
+  void initState() {
+    super.initState();
+  }
   void ApiCallFunction()async{
     final response =
     await http.Client().get(Uri.https('jsonplaceholder.typicode.com','todos/1'));
+
     var output = json.decode(response.body);
     print(output);
     demo1 = Demo.fromJson(output);
     print(demo1.title);
+
+    if (response.statusCode == 200) {
+      return output;
+    } else {
+      throw Exception('Failed to load api call');
+    }
+
+
   }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
 
       home: Scaffold(
-        body:  Column(
-          children: [
-            Center(
-              child: InkWell(onTap: (){
-                ApiCallFunction();
-              },
-                  child: Text('Get Data')),
-            ),
-            Text(demo1.userId.toString()),
-            Text(demo1.id.toString()),
-            Text(demo1.title),
+        body:  Container(
+          margin: EdgeInsets.all(50),
+          child: Column(
+            children: [
+              Center(
+                child: InkWell(onTap: (){
+                  ApiCallFunction();
+                },
+                    child: Text('Get Data')),
+              ),
+              Text(demo1.userId.toString()),
+              Text(demo1.id.toString()),
+              Text(demo1.title),
 
-
-          ],
+            ],
+          ),
         ),
       ),
     );
